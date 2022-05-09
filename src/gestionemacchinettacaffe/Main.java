36e0957416c79be00adda6c5d9b26c2081a32d46
Duke <morgan.d.reis@gmail.com>
2022-05-09 17:54:51 +0200

    Fine

--- src/gestionemacchinettacaffe/Main.java
+++ src/gestionemacchinettacaffe/Main.java
@@ -18,36 +18,42 @@ import java.util.logging.Logger;
 public class Main {
 
     private static Distributore distributore = null;
-    
+
     public static void main(String[] args) {
         distributore = new Distributore();
         System.out.println("Il distributore e' attivo e tutte le bevande sono cariche");
         Bevanda[] bevande = distributore.getBevande();
         Scanner scanner = new Scanner(System.in);
-        do{
+        do {
             //<editor-fold defaultstate="collapsed" desc="MENU PRINCIPALE">
-            System.out.println("Selezionare la bevanda desiderata ↓↓↓"); 
+            System.out.println("Selezionare la bevanda desiderata ↓↓↓");
             System.out.println(" - digita quit per uscire -");
             System.out.println("-------------------------");
             for (Bevanda bevanda : bevande) {
                 System.out.printf("" + bevanda.getId() + ") " + bevanda.getNome() + "\t" + "%.2f\n", bevanda.getPrezzo());
             }
             System.out.println("-------------------------");
-            String scelta = scanner.nextLine();
-            if(scelta.equals("quit")){
+            String scelta = null;
+            try {
+                scelta = scanner.nextLine();
+            } catch (NumberFormatException ex) {
+                System.out.println("Sono ammessi solo numeri");
+                continue;
+            }
+            if (scelta.equals("quit")) {
                 return;
             }
             //</editor-fold>
             int sceltaInt = Integer.parseInt(scelta);
-            if(sceltaInt > 7 || sceltaInt < 1){
+            if (sceltaInt > 7 || sceltaInt < 1) {
                 System.out.println("scelta non valida");
                 continue;
             }
-            Bevanda bevanda = bevande[sceltaInt-1];
+            Bevanda bevanda = bevande[sceltaInt - 1];
             String nomeFinale = bevanda.getNome();
             System.out.println("hai selezionato " + nomeFinale);
             String[] tipologie = bevanda.getTipologie();
-            if(tipologie != null){
+            if (tipologie != null) {
                 System.out.println("seleziona la tipologia ↓↓↓");
                 System.out.println("-------------------------");
                 int id = 1;
@@ -56,15 +62,21 @@ public class Main {
                     id++;
                 }
                 System.out.println("-------------------------");
-                String sottoscelta = scanner.nextLine();
+                String sottoscelta = null;
+                try{
+                    sottoscelta = scanner.nextLine();
+                } catch (NumberFormatException ex) {
+                    System.out.println("Sono ammessi solo numeri");
+                    continue;
+                }
                 int sottosceltaInt = Integer.parseInt(sottoscelta);
-                if(sottosceltaInt < 1 || sottosceltaInt > tipologie.length){
+                if (sottosceltaInt < 1 || sottosceltaInt > tipologie.length) {
                     System.out.println("scelta non valida");
                     continue;
                 }
-                nomeFinale = tipologie[sottosceltaInt-1];
+                nomeFinale = tipologie[sottosceltaInt - 1];
                 System.out.println("hai aggiornato la tua scelta con " + nomeFinale);
-            }    
+            }
             System.out.printf("inserire il seguente importo di € %.2f\n", bevanda.getPrezzo());
             System.out.println("-------------------------");
             int id2 = 1;
@@ -72,47 +84,46 @@ public class Main {
                 System.out.printf("\t> " + id2 + ") € %.2f\n", money.getValue());
                 id2++;
             }
-            
-            
-            
-            
-            
-            
-            System.out.println("La bevanda: " + nomeFinale + " e' in preparazione");
-                for (int i = 0; i < 10; i++) {
-                    System.out.print("#");
-                    try {
-                        Thread.sleep(500);
-                    } catch (InterruptedException ex) {
-                        Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-                    }
+            float soldiInseriti = 0;
+            float resto = 0;
+            while (true) {
+                int soldiId = -1;
+                try {
+                    soldiId = Integer.parseInt(scanner.nextLine());
+                } catch (NumberFormatException ex) {
+                    System.out.println("Sono ammessi solo numeri");
+                    continue;
                 }
-                System.out.println("\nLa bevanda e pronta, premere un tasto per continuare");
-                scanner.nextLine();
-            
-//            switch(sceltaInt){
-//                case 1:{
-//                    
-//                    break;
-//                }
-//                case 2:{
-//                    break;
-//                }
-//                case 3:{
-//                    break;
-//                }
-//                case 4:{
-//                    break;
-//                }
-//                case 5:{
-//                    break;
-//                }
-//                case 6:{
-//                    break;
-//                }
-//            }
-        }while(true);
-        
+                if (soldiId <= 0 || soldiId > MoneyType.values().length) {
+                    System.out.println("scelta non valida");
+                    continue;
+                }
+                MoneyType money = MoneyType.values()[soldiId - 1];
+                System.out.println("hai inserito " + money.getPrintableValue());
+                soldiInseriti += money.getValue();
+                if (soldiInseriti >= bevanda.getPrezzo()) {
+                    resto = soldiInseriti - bevanda.getPrezzo();
+                    if (resto != 0) {
+                        System.out.printf("Hai inserito € %.2f e il resto dovuto e' di € %.2f\n ", soldiInseriti, resto);
+                    }
+                    break;
+                } else {
+                    System.out.printf("totale inserito: € %.2f\n ", soldiInseriti);
+                }
+            }
+            System.out.println("La bevanda: " + nomeFinale + " e' in preparazione");
+            for (int i = 0; i < 10; i++) {
+                System.out.print("#");
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            System.out.println("\nLa bevanda e pronta, premere un tasto per continuare");
+            scanner.nextLine();
+        } while (true);
+
     }
-    
+
 }
--- src/gestionemacchinettacaffe/model/enums/MoneyType.java
+++ src/gestionemacchinettacaffe/model/enums/MoneyType.java
@@ -9,20 +9,28 @@ package gestionemacchinettacaffe.model.enums;
  * @author Megaport
  */
 public enum MoneyType {
-    _10(0.1f),
-    _20(0.2f),
-    _50(0.5f),
-    _1(1.0f),
-    _2(2.0f);
+    _10(0.1f,"10 centesimi"),
+    _20(0.2f,"20 centesimi"),
+    _50(0.5f,"50 centesimi"),
+    _1(1.0f,"1 euro"),
+    _2(2.0f,"2 euro");
     
     private float value;
+    private String printableValue;
 
-    private MoneyType(float value) {
+    private MoneyType(float value, String printableValue) {
         this.value = value;
+        this.printableValue = printableValue;
     }
 
     public float getValue() {
         return value;
     }
 
+    public String getPrintableValue() {
+        return printableValue;
+    }
+
+    
+    
 }
